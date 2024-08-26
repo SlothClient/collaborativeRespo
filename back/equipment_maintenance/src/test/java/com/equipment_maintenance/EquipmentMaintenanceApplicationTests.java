9540218068443908de973939fa0a6a66c5dfc5c5
ys <2900226123@qq.com>
2024-08-26 21:30:27 +0800

    后端项目初始化

---
+++ back/equipment_maintenance/src/test/java/com/equipment_maintenance/EquipmentMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipment_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentMaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
