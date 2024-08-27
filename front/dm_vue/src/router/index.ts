0dceb505c7ea5703ae3079c37ab121aae78b3e48
ys <2900226123@qq.com>
2024-08-27 14:34:07 +0800

    后端项目初始化

--- front/dm_vue/src/router/index.ts
+++ front/dm_vue/src/router/index.ts
@@ -1,26 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Common from '@/views/Common.vue'
 import Device from '@/views/Device.vue'
 import Home from '@/views/Home.vue'
 import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path:'/',
-      component:Common,
-      children:[
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path:'/',
-          component:Home
-        },
-        {path:'/views/Device',
-        component:Device}
+            path: '/',
+            component: Common,
+            children: [
+                {
+                    path: '/',
+                    component: Home
+                },
+                {
+                    path: '/views/Device',
+                    component: Device
+                }
 
-      ]
-     }
+            ]
+        }
 
-  ]
+    ]
 })
 
 export default router
