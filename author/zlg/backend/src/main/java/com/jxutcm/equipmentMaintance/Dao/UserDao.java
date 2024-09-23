0f4a7b192138947e4c049cdff5bc7a2d11ad7b70
ys <2900226123@qq.com>
2024-09-23 16:55:44 +0800

    无

--- author/zlg/backend/src/main/java/com/jxutcm/equipmentMaintance/Dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.jxutcm.equipmentMaintance.Dao;
-
-import com.jxutcm.equipmentMaintance.Bean.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao {
-    public User doLogin(User user);
-}
--- author/zlg/backend/src/main/java/com/jxutcm/equipmentMaintance/Service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.jxutcm.equipmentMaintance.Service;
-
-import com.jxutcm.equipmentMaintance.Bean.User;
-
-public interface UserService {
-    public User doLogin(String uname, String upwd);
-}
